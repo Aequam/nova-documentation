60f20e5d2bf5a1af744bb4b76452a4b39e2d0dee
Bartosz Bujak (mac) <bartosz.bujak@me.com>
2020-10-07 13:26:01 +0200

    Aequam visual tweaks

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -1,28 +1,32 @@
 <template>
-    <card class="doc-order-first md:doc-order-3 doc-w-full md:doc-w-2/5 lg:doc-w-4/12 xl:doc-w-4/12 bg-white p-6 self-start mb-3 md:doc-mb-0">
-        <h3 class="mb-2">Contents</h3>
-        <ul>
-            <li class="mb-2" v-for="item of content">
-                <router-link :to="{name: item.name}"
-                             class="cursor-pointer flex items-center font-normal text-black dim text-base no-underline"
-                             exact
-                >{{ item.title }}</router-link>
-            </li>
-        </ul>
-    </card>
+  <card
+    class="doc-order-first md:doc-order-3 doc-w-full md:doc-w-2/5 lg:doc-w-4/12 xl:doc-w-4/12 bg-white p-6 self-start mb-3 md:doc-mb-0"
+  >
+    <h3 class="mb-2">Contents</h3>
+    <ul>
+      <li class="mb-2" v-for="item of content">
+        <router-link
+          :to="{ name: item.name }"
+          class="cursor-pointer flex items-center font-normal text-black dim text-base no-underline"
+          exact
+          >{{ item.title }}</router-link
+        >
+      </li>
+    </ul>
+  </card>
 </template>
 
 <script>
-    export default {
-        name: "Sidebar",
-        props: {
-            content: Array
-        }
-    }
+export default {
+  name: "Sidebar",
+  props: {
+    content: Array,
+  },
+};
 </script>
 
 <style scoped>
-    .router-link-exact-active.router-link-active {
-        color: var(--primary);
-    }
+.router-link-exact-active.router-link-active {
+  color: var(--primary);
+}
 </style>
